427604fd96085ea2b87d878b0cdf773b16bd78b3
Xavier Hainaux <xavier.hainaux@gmail.com>
2022-06-21 14:20:19 +0200

    Add test workflow

--- test/all_pub_metrics_test.dart
+++ test/all_pub_metrics_test.dart
@@ -1,7 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:all_pub_metrics/all_pub_metrics.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('empty test', () {
+  test('Test read file', () async {
+    var dataPath = 'data/all_packages.json';
 
+    var content = File(dataPath).readAsStringSync();
+    var json = jsonDecode(content) as Map<String, dynamic>;
+    var packages = Packages.fromJson(json);
+
+    expect(packages.packages, isNotEmpty);
   });
 }
